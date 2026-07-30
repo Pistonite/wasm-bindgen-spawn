bea9829b9d0c94f923e6a623d3464da95272b1e9
BadMachine <iskullbreakeri@gmail.com>
2026-07-31 01:05:00 +0200

    fix: remove redundant config file

--- build.mjs
+++ build.mjs
@@ -1,26 +1,29 @@
 import * as esbuild from "esbuild";
-import { inlinePlugin } from './inline.plugin.mjs';
+import {inlinePlugin} from './inline.plugin.mjs';
 
+const isWeb = process.argv.includes('--web');
+const target = isWeb ? 'web' : 'no_modules';
 
 await esbuild.build({
 	entryPoints: [
-		"src/ts/web/index.ts"
+		`src/ts/${target}/index.ts`
 	],
 	bundle: true,
 	splitting: false,
 	treeShaking: true,
 	minify: true,
 	format: "iife",
-	platform: "browser",
+	platform: isWeb ? "browser" : undefined,
 	globalName: "__wbgSpawn",
 	footer: {
 		js: "return __wbgSpawn.default.apply(null, args);",
 	},
-	outfile: "./src/ts/dist/createDispatcher.web.min.js",
+	outfile: `./src/ts/dist/createDispatcher.${target}.min.js`,
 	plugins: [
 		inlinePlugin({
 			format: "esm",
-			platform: "browser"
+			platform: "browser",
+			raw: !isWeb
 		})
 	],
 	loader: {
